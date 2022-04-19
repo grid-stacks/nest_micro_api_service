2addc7e48a2e8b22aea16ebbd608d4adc204cbe9
DHN Chandan <php.chandan@gmail.com>
2022-04-19 20:08:14 +0600

    Update auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,43 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Body,
+  Controller,
+  OnModuleInit,
+  Post,
+  Put,
+  Inject,
+} from '@nestjs/common';
+import {
+  LoginRequest,
+  RegisterRequest,
+  RegisterResponse,
+  LoginResponse,
+  AUTH_SERVICE_NAME,
+  AuthServiceClient,
+} from './auth.pb';
+import { Observable } from 'rxjs';
+import { ClientGrpc } from '@nestjs/microservices';
 
 @Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+export class AuthController implements OnModuleInit {
+  private service: AuthServiceClient;
+
+  @Inject(AUTH_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.service = this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+  @Post('register')
+  private async register(
+    @Body() body: RegisterRequest,
+  ): Promise<Observable<RegisterResponse>> {
+    return this.service.register(body);
+  }
+
+  @Put('login')
+  private async login(
+    @Body() body: LoginRequest,
+  ): Promise<Observable<LoginResponse>> {
+    return this.service.login(body);
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AUTH_PACKAGE_NAME, AUTH_SERVICE_NAME } from './auth.pb';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: AUTH_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50051',
+          package: AUTH_PACKAGE_NAME,
+          protoPath: 'node_modules/nest_micro_proto/proto/auth.proto',
+        },
+      },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
